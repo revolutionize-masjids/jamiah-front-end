7ccc0e2c6b51f9460dab5dd57e47d4466b742f55
Syed Aman <sna274@nyu.edu>
2017-08-03 16:55:59 -0500

    add in a router-view to harness so that it can render other components like Home inside

--- src/components/Harness.vue
+++ src/components/Harness.vue
@@ -1,25 +1,29 @@
 <template>
-  <md-toolbar class="md-large">
-    <div class="md-toolbar-container">
-      <md-button class="md-icon-button">
-        <md-icon>menu</md-icon>
-      </md-button>
+  <div>
+    <md-toolbar class="md-large">
+      <div class="md-toolbar-container">
+        <md-button class="md-icon-button">
+          <md-icon>menu</md-icon>
+        </md-button>
 
-      <span style="flex: 1;"></span>
+        <span style="flex: 1;"></span>
 
-      <md-button class="md-icon-button">
-        <md-icon>search</md-icon>
-      </md-button>
+        <md-button class="md-icon-button">
+          <md-icon>search</md-icon>
+        </md-button>
 
-      <md-button class="md-icon-button">
-        <md-icon>filter_list</md-icon>
-      </md-button>
-    </div>
+        <md-button class="md-icon-button">
+          <md-icon>filter_list</md-icon>
+        </md-button>
+      </div>
 
-    <div class="md-toolbar-container">
-      <h2 class="md-title">East Elmhurst Jam-E Muslim Center</h2>
-    </div>
-  </md-toolbar>
+      <div class="md-toolbar-container">
+        <h2 class="md-title">East Elmhurst Jam-E Muslim Center</h2>
+      </div>
+    </md-toolbar>
+    <router-view></router-view>
+  </div>
+
 </template>
 
 <script>
