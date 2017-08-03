245dd196193fc876cab0230f8a2a77315d6b3dd3
Syed Aman <sna274@nyu.edu>
2017-08-03 16:48:44 -0500

    remove boilerplate cruft from Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,34 +1,13 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+    <h1>Home</h1>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
-</script>
-
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+<script></script>
+<style></style>
