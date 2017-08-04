0f9732b684afb9d14a0b547a2632ee0945d4cd92
Syed Aman <sna274@nyu.edu>
2017-08-04 18:48:16 -0500

    convert the prayer schedules from a list to a table

--- src/components/PrayerSchedule.vue
+++ src/components/PrayerSchedule.vue
@@ -2,69 +2,93 @@
 
 <template>
   <md-card class="prayer-schedule md-accent">
+
+    <!-- Content Above Prayer Times -->
     <md-card-header>
       <div class="masjid-image">
         <md-image :md-src="MasjidImage" />
       </div>
       <md-toolbar class="md-primary">
-        <span class="md-title">Jama'at Times</span>
+        <span class="md-title">Prayer Times</span>
       </md-toolbar>
     </md-card-header>
+
+    <!-- Table displaying Jama'at Times -->
     <md-card-content>
-      <md-list>
-        <!-- Fajr -->
-        <md-list-item>
-          <md-avatar>
-            <md-image :md-src="SunriseImage" alt="People" />
-          </md-avatar>
-          <span>Fajr</span>
-          <span>5:30 AM</span>
-        </md-list-item>
+      <md-table>
+        <!-- Column labels -->
+        <md-table-header>
+          <md-table-row>
+            <md-table-head></md-table-head>
+            <md-table-head>Salah</md-table-head>
+            <md-table-head>Adhan</md-table-head>
+            <md-table-head>Iqaamah</md-table-head>
+          </md-table-row>
+        </md-table-header>
 
-        <md-divider></md-divider>
+        <!-- Table content -->
+        <md-table-body>
+          <!-- Fajr -->
+          <md-table-row>
+            <md-table-cell>
+              <md-avatar>
+                <md-image :md-src="SunriseImage" alt="People" />
+              </md-avatar>
+            </md-table-cell>
+            <md-table-cell>Fajr</md-table-cell>
+            <md-table-cell>5:15 AM</md-table-cell>
+            <md-table-cell>5:30 AM</md-table-cell>
+          </md-table-row>
 
-        <!-- Dhuhr -->
-        <md-list-item>
-          <md-avatar>
-            <md-image :md-src="MidDayImage" alt="People"/></md-image>
-          </md-avatar>
-          <span>Dhuhr</span>
-          <span>1:30 PM</span>
-        </md-list-item>
+          <!-- Dhuhr -->
+          <md-table-row>
+            <md-table-cell>
+              <md-avatar>
+                <md-image :md-src="MidDayImage" alt="People" />
+              </md-avatar>
+            </md-table-cell>
+            <md-table-cell>Dhuhr</md-table-cell>
+            <md-table-cell>1:15 PM</md-table-cell>
+            <md-table-cell>1:30 PM</md-table-cell>
+          </md-table-row>
 
-        <md-divider></md-divider>
+          <!-- 'Asr -->
+          <md-table-row>
+            <md-table-cell>
+              <md-avatar>
+                <md-image :md-src="AfternoonImage" alt="People" />
+              </md-avatar>
+            </md-table-cell>
+            <md-table-cell>'Asr</md-table-cell>
+            <md-table-cell>5:15 PM</md-table-cell>
+            <md-table-cell>5:30 PM</md-table-cell>
+          </md-table-row>
 
-        <!-- 'Asr -->
-        <md-list-item>
-          <md-avatar>
-            <md-image :md-src="AfternoonImage" alt="People"/></md-image>
-          </md-avatar>
-          <span>'Asr</span>
-          <span>4:30 PM</span>
-        </md-list-item>
+          <!-- Maghrib -->
+          <md-table-row>
+            <md-table-cell>
+              <md-avatar>
+                <md-image :md-src="SunsetImage" alt="People" />
+              </md-avatar>
+            </md-table-cell>
+            <md-table-cell>Maghrib</md-table-cell>
+            <md-table-cell>8:27 PM</md-table-cell>
+            <md-table-cell>8:27 PM</md-table-cell>
+          </md-table-row>
 
-        <md-divider></md-divider>
-
-        <!-- Maghrib -->
-        <md-list-item>
-          <md-avatar>
-            <md-image :md-src="SunsetImage" alt="People"/></md-image>
-          </md-avatar>
-          <span>Maghrib</span>
-          <span>8:00 PM</span>
-        </md-list-item>
-
-        <md-divider></md-divider>
-
-        <!-- 'Isha -->
-        <md-list-item>
-          <md-avatar>
-            <md-image :md-src="NightImage" alt="People"/></md-image>
-          </md-avatar>
-          <span>'Isha</span>
-          <span>10:30 PM</span>
-        </md-list-item>
-      </md-list>
+          <!-- Isha' -->
+          <md-table-row>
+            <md-table-cell>
+              <md-avatar>
+                <md-image :md-src="NightImage" alt="People" />
+              </md-avatar>
+            </md-table-cell>
+            <md-table-cell>'Isha</md-table-cell>
+            <md-table-cell>10:15 PM</md-table-cell>
+            <md-table-cell>10:30 PM</md-table-cell>
+          </md-table-row>
+        </md-table-body>
+      </md-table>
     </md-card-content>
   </md-card>
 </template>
@@ -101,4 +125,8 @@
 
         .md-image
           width: 40%
+
+    .md-card-content
+      .md-table
+        background-color: white
 </style>
