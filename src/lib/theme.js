79572ba0f3d99d643fa9f21352074aee077dbf00
Syed Aman <sna274@nyu.edu>
2017-08-04 08:33:57 -0500

    register app-wide theme as teal with an accent color that is a little darker hue of teal

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -9,3 +9,18 @@ import 'vue-material/dist/vue-material.css'
 
 // Use the whole Vue Material library
 Vue.use(VueMaterial)
+
+// define theme
+const base = {
+  primary: {
+    color: 'teal',
+    hue: 500
+  },
+  accent: {
+    color: 'teal',
+    hue: 800
+  },
+  warn: 'red'
+}
+
+Vue.material.registerTheme('default', base)
