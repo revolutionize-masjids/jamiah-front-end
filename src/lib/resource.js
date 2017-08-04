7a0415c3f0c0b7cabe5e27cf3a47a2a7c7b1839e
Syed Aman <sna274@nyu.edu>
2017-08-03 19:32:33 -0500

    use VueAuthenticate for social media (facebook) authentication

--- src/lib/resource.js
+++ src/lib/resource.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueAuthenticate from 'vue-authenticate'
 
 // use official Vue HTTP client to make web requests and handle responses using a XMLHttpRequest or JSONP
 Vue.use(VueResource)
+
+Vue.use(VueAuthenticate, {
+  baseUrl: 'https://eejmc.org:6236', // Your API domain
+
+  providers: {
+    facebook: {
+      clientId: '1857368264592327',
+      redirectUri: 'https://eejmc.org:6236' // Your client app URL
+    }
+  }
+})
