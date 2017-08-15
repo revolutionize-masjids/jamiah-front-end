22d2cccef772032caad42991b811c8b04ed38ab6
Syed Aman <sna274@nyu.edu>
2017-08-15 18:25:59 -0500

    improve styling of signup card

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,24 +1,40 @@
 <!-- How users will be able to sign up to the app. They can use social media accounts like Facebook and possibly make their own accounts using custom email and password. -->
 
 <template>
-  <div>
+  <md-layout root-element>
     <!-- Signup with Facebook -->
     <!-- <md-button @click.native="authenticate('facebook')">
       Signup with Facebook
     </md-button> -->
-    <md-subheader>Start engaging with your own community!</md-subheader>
-    <form novalidate @submit.stop.prevent="submit">
-      <md-input-container>
-        <label>Username</label>
-        <md-input v-model="username"></md-input>
-      </md-input-container>
+    <md-layout md-align="center" md-vertical-align="center">
+      <md-card>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">Sign Up</div>
+            <div class="subhead">Start engaging with your own community!</div>
+          </md-card-header-text>
+        </md-card-header>
+        <md-card-content>
+          <form novalidate @submit.stop.prevent="submit">
+            <md-input-container>
+              <label>Username</label>
+              <md-input v-model="username"></md-input>
+            </md-input-container>
 
-      <md-input-container md-has-password>
-        <label>Password</label>
-        <md-input type="password" v-model="password"></md-input>
-      </md-input-container>
-    </form>
-  </div>
+            <md-input-container md-has-password>
+              <label>Password</label>
+              <md-input type="password" v-model="password"></md-input>
+            </md-input-container>
+          </form>
+        </md-card-content>
+        <md-card-actions>
+          <md-layout md-align="end">
+            <md-button>Sign Up</md-button>
+          </md-layout>
+        </md-card-actions>
+      </md-card>
+    </md-layout>
+  </md-layout>
 </template>
 
 <script>
@@ -45,4 +61,7 @@
   }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+  [root-element]
+    height: 100vh
+</style>
