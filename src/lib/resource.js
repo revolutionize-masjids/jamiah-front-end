9ad095c5ec9ce924900fd6fef0f04fa023d01dec
Syed Aman <sna274@nyu.edu>
2017-08-24 12:37:24 -0500

    edit vue authentication config

--- src/lib/resource.js
+++ src/lib/resource.js
@@ -4,19 +4,21 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VueAuthenticate from 'vue-authenticate'
 
+import config from '@/config'
+
 // use official Vue HTTP client to make web requests and handle responses using a XMLHttpRequest or JSONP
 Vue.use(VueResource)
 
 // use vue-authenticate for social media authentication
 Vue.use(VueAuthenticate, {
-  baseUrl: 'https://eejmc.org:6236', // Your API domain
+  baseUrl: config.api + '/grapql', // Your API domain
 
   // all the social media sites we support for authentication
   providers: {
     facebook: {
       // clientId fetched from FB's dev tools after registering the app's domain to use social media authentication
       clientId: '1857368264592327',
-      redirectUri: 'https://eejmc.org:6236'
+      redirectUri: 'http://localhost:6236'
     }
   }
 })
