0836108f42aeaa85908a1419731b11b6f1d37636
Syed Aman <sna274@nyu.edu>
2017-08-25 12:08:34 -0500

    modularize the facebook login button

---
+++ src/components/common/social-media-buttons/FacebookLoginButton.vue
@@ -0,0 +1,39 @@
+<!-- Button that allows user to log in or sign up via Facebook -->
+
+<template>
+  <md-button class="md-raised facebook-signup-button social-media-button" @click.native="authenticate('facebook')">
+    <md-layout>
+      <md-layout md-align="center" md-flex="10" md-flex-offset-xlarge="10">
+        <md-icon :md-src="facebookLogo"></md-icon>
+      </md-layout>
+      <md-layout md-align="center" class="button-text">
+        <span>Sign up with Facebook</span>
+      </md-layout>
+    </md-layout>
+  </md-button>
+</template>
+
+<script>
+  import facebookLogo from '@/assets/icons/facebook-logo.svg'
+
+  export default {
+    methods: {
+      // Use the Vue-Authenticate package to authenticate account via social
+      // media
+      async authenticate (provider) {
+        try {
+          await this.$auth.authenticate(provider)
+          // Execute application logic after successful social authentication
+          console.log('successfully authenticated')
+        } catch (error) {
+          console.log('error authenticating', error)
+        }
+      }
+    },
+    data: function () {
+      return ({
+        facebookLogo
+      })
+    }
+  }
+</script>
