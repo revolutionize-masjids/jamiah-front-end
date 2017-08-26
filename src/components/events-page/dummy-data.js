cbcafad9917c8af81487b603947dada4e09287e2
Syed Aman <sna274@nyu.edu>
2017-08-26 13:32:54 -0500

    create dummy data

---
+++ src/components/events-page/dummy-data.js
@@ -0,0 +1,56 @@
+const events = [
+  {
+    name: 'Annual International Art Exhibit',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    dateStart: null,
+    dateEnd: null,
+    address: '42-31 Lexington Ave, New York 10000',
+    attendees: 0,
+    likes: 0,
+    isNotificationsOn: true,
+    comments: [
+      {
+        name: 'Ali Afzar',
+        postedElapsedtime: 3,
+        comment: 'Salaam brothers and sisters! In order to hose this event, we need a total of 12 volunteers. Please click the link above so that we can receive the aid from you. Thanks!',
+        likes: 2,
+        didUserLike: false
+      },
+      {
+        name: 'Mohammed Yaseen',
+        postedElapsedtime: 5,
+        comment: 'Hey I can volunteer inshAllah!',
+        likes: 8,
+        didUserLike: true
+      }
+    ]
+  },
+  {
+    name: 'Eid al-Adha Qurbani',
+    description: 'We are all going to Pennsylvania for Qurbani; slaughtering cows and goats. If you guys want to come with us, just RSVP. Fees are costs for slaughtering an animal in addition to transportation fees.',
+    dateStart: null,
+    dateEnd: null,
+    address: 'Random Farm, PA',
+    attendees: 15,
+    likes: 6,
+    isNotificationsOn: false,
+    comments: [
+      {
+        name: 'Shadab Hassan',
+        postedElapsedtime: 2,
+        comment: 'Yeeeee boi',
+        likes: 2,
+        didUserLike: false
+      },
+      {
+        name: 'Farshed Adib',
+        postedElapsedtime: 5,
+        comment: 'Going',
+        likes: 8,
+        didUserLike: true
+      }
+    ]
+  }
+]
+
+export default events
