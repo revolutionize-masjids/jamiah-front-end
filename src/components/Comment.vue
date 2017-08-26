c371d5b925b28049682fe2de70ccbaa4786a848c
Syed Aman <sna274@nyu.edu>
2017-08-26 12:15:43 -0500

    create a comment component

---
+++ src/components/Comment.vue
@@ -0,0 +1,37 @@
+<template>
+  <md-list-item>
+    <md-avatar>
+      <img src="https://placeimg.com/40/40/people/1" alt="People">
+    </md-avatar>
+
+    <div class="md-list-text-container">
+      <span>{{ name }}</span>
+      <span>Posted {{postedElapsedtime }} days ago</span>
+      <p>{{ comment }}</p>
+      <span><md-icon>favorite</md-icon> {{ likes }}</span>
+    </div>
+
+    <md-button class="md-icon-button md-list-action">
+      <md-icon class="md-primary" v-if="didUserLike">favorite</md-icon>
+      <md-icon class="md-primary" v-else>favorite_border</md-icon>
+    </md-button>
+
+    <md-divider class="md-inset"></md-divider>
+  </md-list-item>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return ({
+        name: 'Ali Afzar',
+        postedElapsedtime: 3,
+        comment: 'Salaam brothers and sisters! In order to hose this event, we need a total of 12 volunteers. Please click the link above so that we can receive the aid from you. Thanks!',
+        likes: 2,
+        didUserLike: true
+      })
+    }
+  }
+</script>
+
+<style lang="sass"></style>
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -91,79 +91,8 @@
         </form>
 
         <!-- Comments list -->
-        <md-list class="custom-list md-triple-line">
-          <md-list-item>
-            <md-avatar>
-              <img src="https://placeimg.com/40/40/people/1" alt="People">
-            </md-avatar>
-
-            <div class="md-list-text-container">
-              <span>Ali Afzar</span>
-              <span>Posted 3 days ago</span>
-              <p>Salaam brothers and sisters! In order to hose this event, we need a total of 12 volunteers. Please click the link above so that we can receive the aid from you. Thanks!</p>
-              <span><md-icon>favorite</md-icon> 2</span>
-            </div>
-
-            <md-button class="md-icon-button md-list-action">
-              <md-icon class="md-primary">favorite</md-icon>
-            </md-button>
-
-            <md-divider class="md-inset"></md-divider>
-          </md-list-item>
-
-          <md-list-item>
-            <md-avatar>
-              <img src="https://placeimg.com/40/40/people/6" alt="People">
-            </md-avatar>
-
-            <div class="md-list-text-container">
-              <span>me, Scott, Jennifer</span>
-              <span>Summer BBQ</span>
-              <p>Wish I could come, but I'm out of town ...</p>
-            </div>
-
-            <md-button class="md-icon-button md-list-action">
-              <md-icon>favorite_border</md-icon>
-            </md-button>
-
-            <md-divider class="md-inset"></md-divider>
-          </md-list-item>
-
-          <md-list-item>
-            <md-avatar>
-              <img src="https://placeimg.com/40/40/people/5" alt="People">
-            </md-avatar>
-
-            <div class="md-list-text-container">
-              <span>Sandra Adams</span>
-              <span>Oui oui</span>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-
-            <md-button class="md-icon-button md-list-action">
-              <md-icon>favorite_border</md-icon>
-            </md-button>
-
-            <md-divider class="md-inset"></md-divider>
-          </md-list-item>
-
-          <md-list-item>
-            <md-avatar>
-              <img src="https://placeimg.com/40/40/people/8" alt="People">
-            </md-avatar>
-
-            <div class="md-list-text-container">
-              <span>Trevor Hansen</span>
-              <span>Order confirmation</span>
-              <p>Thank you for your recent order from ...</p>
-            </div>
-
-            <md-button class="md-icon-button md-list-action">
-              <md-icon>favorite_border</md-icon>
-            </md-button>
-
-            <md-divider class="md-inset"></md-divider>
-          </md-list-item>
+        <md-list class="md-triple-line">
+          <Comment />
         </md-list>
       </md-layout>
     </md-layout>
@@ -171,7 +100,13 @@
 </template>
 
 <script>
-  export default {}
+  import Comment from '@/components/Comment'
+
+  export default {
+    components: {
+      Comment
+    }
+  }
 </script>
 
 <style lang="sass">
