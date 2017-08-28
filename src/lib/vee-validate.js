c32025dc9d4069919abbe161c108cf1a4bded130
Syed Aman <sna274@nyu.edu>
2017-08-28 06:32:09 -0500

    debounce validation errors for 500 milliseconds

--- src/lib/vee-validate.js
+++ src/lib/vee-validate.js
@@ -1,9 +1,16 @@
-// use and configure VeeValidate package for form validation
+/**
+ * Use and configure Vee Validate to use the v-validate directive to validate
+ * forms before they are sent to the API. Read http://vee-validate.logaretm.com
+ * to learn more.
+ */
 
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 
-// define plugin configuration (http://vee-validate.logaretm.com/#configuration)
-const config = {}
+/** configure how forms are validated */
+const config = {
+  // debounce validation errors for 500 milliseconds
+  delay: 500
+}
 
 Vue.use(VeeValidate, config)
