2561c26bfa6778e1c67c34915272404536aca549
Syed Aman <sna274@nyu.edu>
2017-09-19 14:09:38 -0500

    display the navigation toolbar

---
+++ components/NavToolbar/NavToolbar.vue
@@ -0,0 +1,16 @@
+<!-- The top navigation toolbar contains essential navigation controls and
+action buttons. -->
+
+<template>
+  <v-toolbar class="primary" dark dense fixed>
+    <!-- Hamburger menu that open the navigation drawer. -->
+    <v-toolbar-side-icon @click.native="emitOpenNavDrawerEvent()" dark />
+
+    <!-- Company logo button that takes user to homepage. -->
+    <v-toolbar-title>
+      <router-link :to="{ name: 'Home' }" tag="span" router-link-inactive>
+        Jamiah
+      </router-link>
+    </v-toolbar-title>
+  </v-toolbar>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -22,35 +22,7 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <NavToolbar></NavToolbar>
     <main>
       <v-container fluid>
         <nuxt />
@@ -77,7 +49,12 @@
 </template>
 
 <script>
+  import NavToolbar from '../components/NavToolbar/NavToolbar.vue'
+
   export default {
+    components: {
+      NavToolbar
+    },
     data () {
       return {
         clipped: false,
