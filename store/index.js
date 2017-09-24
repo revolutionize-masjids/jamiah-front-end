657add5556d40faaadbe3b50ea128fc80cb95384
Syed Aman <sna274@nyu.edu>
2017-09-24 17:31:48 -0500

    convert vue store to SSR

--- store/index.js
+++ store/index.js
@@ -1,9 +1,31 @@
 export const state = () => ({
-  sidebar: false
+  /** whether user is logged in or not */
+  isLoggedIn: false,
+  /** whether the login/signup dialog is open */
+  isLoginDialogOpen: false,
+  /** general user information */
+  user: {
+    userId: null,
+    firstName: null,
+    lastName: null,
+    email: null,
+    /** profile picture */
+    avatar: null
+  }
 })
 
 export const mutations = {
-  toggleSidebar (state) {
-    state.sidebar = !state.sidebar
+  /** update app state to reflect that user logged in */
+  login (state, payload) {
+    state.isLoggedIn = true
+    state.user = payload
+  },
+  /** update app state to reflect that user logged out */
+  logout (state) {
+    state.isLoggedIn = false
+  },
+  /** open or close the login/signup dialog */
+  setIsLoginDialogOpen (state, payload) {
+    state.isLoginDialogOpen = payload
   }
 }
