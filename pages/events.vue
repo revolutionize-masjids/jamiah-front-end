23c76e9cd65c1cfb6484af4a5d0a09fbcf46b485
Syed Aman <sna274@nyu.edu>
2017-09-26 14:37:21 -0500

    begin adding in the event cards

---
+++ pages/events.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card class="pa-2">
+    <v-layout column>
+      <v-flex>
+        <v-layout row>
+          <v-flex xs4 class="indigo lighten-3 text-xs-center white--text">
+            <!-- TODO: improve SEO -->
+            <h5 class="headline">15</h5>
+            <h5 class="title">June</h5>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-card>
+</template>
