5c6cd29ab9552c28bea47f662805f14cfafbd4c3
Syed Aman <sna274@nyu.edu>
2017-09-26 15:12:10 -0500

    attempt to make a GraphQL query in the events page

--- pages/events.vue
+++ pages/events.vue
@@ -13,3 +13,69 @@
     </v-layout>
   </v-card>
 </template>
+
+<script>
+  import gql from 'graphql-tag'
+
+  export default {
+    data: function () {
+      return ({
+        /** all events to display */
+        allEvents: null
+      })
+    },
+    methods: {
+      /** update the view with fresh data */
+      reload () {
+        // update the view when an event is created with new data
+      }
+    },
+    /** define GraphQL requests & interface with API */
+    apollo: {
+      // fetch all created events and store them in the events property
+      allEvents: gql`
+        query {
+          allEvents {
+            _id
+            name
+            description
+            address
+            attendees {
+              firstName
+              lastName
+            }
+            likes {
+              firstName
+              lastName
+            }
+            subscribedUsers {
+              firstName
+              lastName
+            }
+            volunteers {
+              firstName
+              lastName
+            }
+            # get comments for the comment section of the event card
+            comments {
+              body
+              commenter {
+                firstName
+                lastName
+                avatar {
+                  imageUrl
+                }
+              }
+              created
+              lastUpdated
+              likes {
+                firstName
+                lastName
+              }
+            }
+          }
+        }
+      `
+    }
+  }
+</script>
