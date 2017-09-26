a20a2791f95b8a4b0b634bbd5495db99ee68cc7f
Syed Aman <sna274@nyu.edu>
2017-09-26 09:10:30 -0500

    improve documentation for the login component

--- components/NavToolbar/Login.vue
+++ components/NavToolbar/Login.vue
@@ -1,4 +1,4 @@
-<!-- The login/signup dialog with social media logins. -->
+<!-- The login/signup dialog with social media logins to login to the app. -->
 
 <template>
   <!-- Dialog is fullscreen on mobile and normal on larger screens. -->
@@ -19,35 +19,35 @@
 
       <!-- Main content of dialog. -->
       <v-card-text>
-        <!-- Login form that validates credentials -->
+        <!-- Login form that validates credentials. -->
         <form @submit.prevent="login()">
           <v-layout column>
-            <!-- Email -->
+            <!-- Email. -->
             <v-text-field v-model="emailInput" v-validate="'required|email'" label="Email" :error-messages="errors.collect('email')" type="email" name="email" prepend-icon="mail" required></v-text-field>
 
-            <!-- Save session to local storage -->
+            <!-- Save session to local storage. -->
             <v-checkbox v-model="shouldRemainSignedIn" :label="`Stay signed on`" class="pt-0"></v-checkbox>
 
-            <!-- Password -->
+            <!-- Password. -->
             <v-text-field v-model="passwordInput" v-validate="'required|alpha_dash|min:6'" :error-messages="errors.collect('password')" label="Password" type="password" name="password" prepend-icon="lock" required class="mb-5"></v-text-field>
 
-            <!-- Form submit button that prompts API to authenticate -->
-            <v-btn type="submit" dark raised class="primary ">Login</v-btn>
+            <!-- Form submit button that prompts API to authenticate. -->
+            <v-btn type="submit" dark raised class="primary">Login</v-btn>
 
-            <!-- Beginning of social media logins -->
+            <!-- Beginning of social media logins. -->
             <v-divider class="mt-3 mb-3"></v-divider>
 
-            <!-- Facebook login -->
+            <!-- Facebook login. -->
             <v-btn class="blue darken-3" dark raised>
               Login with Facebook
             </v-btn>
 
-            <!-- Google+ Login -->
+            <!-- Google+ Login. -->
             <v-btn class="red lighten-2" dark raised>
               Login with Google+
             </v-btn>
 
-            <!-- Redirect user to signup page -->
+            <!-- Redirect user to signup page. -->
             <p class="md-caption">
               <span>New User?</span>
               <router-link to="signup" @click.native="closeDialog()">
@@ -65,16 +65,29 @@
   export default {
     data: function () {
       return ({
-        /** the value for the email input in the login form */
+        /**
+         * Stores user form input representing user email for logging in.
+         * @type {String}
+         */
         emailInput: null,
-        /** the value for the password input in the login form */
+        /**
+         * Stores user form input representing secure password.
+         * @type {String}
+         */
         passwordInput: null,
-        /** whether the user wants to remain signed in */
+        /**
+         * Stores user form input representing whether user wants to remain
+         * signed in.
+         * @type {Boolean}
+         */
         shouldRemainSignedIn: false
       })
     },
     computed: {
-      /** whether the dialog is be open or closed */
+      /**
+       * Fetches or sets data controling whether dialog is open or closed.
+       * @type {Object}
+       */
       isLoginDialogOpen: {
         get () {
           return this.$store.state.isLoginDialogOpen
@@ -86,10 +99,20 @@
     },
     methods: {
       /** close the login dialog for whatever reason */
+      /**
+       * Closes the login dialog.
+       * @method closeDialog
+       * @return {Void}
+       */
       closeDialog () {
         this.$store.commit('setIsLoginDialogOpen', false)
       },
-      /** authenticate user and login */
+      /**
+       * Authenticates user with the API and then logs user into the app.
+       * @method login
+       * @return {Void}
+       * @throws {Error}
+       */
       async login () {
         try {
           // see if form inputs are valid
@@ -104,7 +127,10 @@
             firstName: null,
             lastName: null,
             email: this.emailInput,
-            /** profile picture */
+            /**
+             * The user's profile picture object data.
+             * @type {Object}
+             */
             avatar: null
           })
         } catch (error) {
