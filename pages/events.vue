1646c1347c698436fc0b0f75b1870f752d595a23
Syed Aman <sna274@nyu.edu>
2017-09-26 15:40:23 -0500

    loop through the events data fetched from the api

--- pages/events.vue
+++ pages/events.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-card class="pa-2">
-    <v-layout column>
-      <v-flex>
-        <v-layout row>
-          <v-flex xs4 class="indigo lighten-3 text-xs-center white--text">
-            <!-- TODO: improve SEO -->
-            <h5 class="headline">15</h5>
-            <h5 class="title">June</h5>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-card>
+  <v-layout column>
+    <v-card v-for="event in allEvents" :key="event._id" class="pa-2 mb-4">
+      <v-layout column>
+        <v-flex>
+          <v-layout row>
+            <v-flex xs4 class="indigo lighten-3 text-xs-center white--text">
+              <!-- TODO: improve SEO -->
+              <h5 class="headline">15</h5>
+              <h5 class="title">June</h5>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-card>
+  </v-layout>
 </template>
 
 <script>
