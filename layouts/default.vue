2661218dd0b7ee50c28c03845bc3c496e603f498
Syed Aman <sna274@nyu.edu>
2017-09-26 09:58:38 -0500

    remove leftover boilerplate code

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,21 +22,6 @@
     components: {
       NavToolbar,
       NavDrawer
-    },
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { to: '/', title: 'Welcome', icon: 'apps' },
-          { to: '/inspire', title: 'Inspire', icon: 'bubble_chart' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
     }
   }
 </script>
