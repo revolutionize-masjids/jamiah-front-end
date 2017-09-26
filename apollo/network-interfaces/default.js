96e2a9f78e582db6f3383676630192481b19cc57
Syed Aman <sna274@nyu.edu>
2017-09-26 14:48:49 -0500

    hardcode in the environment-based-endpoint for apollo

--- apollo/network-interfaces/default.js
+++ apollo/network-interfaces/default.js
@@ -1,18 +1,18 @@
 import { createBatchingNetworkInterface } from 'apollo-client'
 
 export default (ctx) => {
-  // TODO: figure out how to use environment variables in Nuxt.js
-  console.log(process.env[process.env.NODE_ENV])
   /**
    * Apollo client network interface options.
    * @type {[type]}
    */
   const networkInterface = createBatchingNetworkInterface({
+    // TODO: figure out how to use environment variables in Nuxt.js instead of
+    // hardcoding the environment-based-endpoint
     /**
        * GraphQL API url based on the environment: dev, prod, etc.
        * @type {String}
        */
-    uri: process.env[process.env.NODE_ENV] + '/graphql'
+    uri: process.env.production.api + '/graphql'
   })
 
   return networkInterface
